68350b9e3e3b11111388143b0be89ebd0f27b016
mahaboobfarooq01-ux <mahaboobfarooq01@gmail.com>
2026-07-04 13:54:06 +0530

    Rename chameleon image to bypass browser caching and ensure transparency update is visible

--- artifacts/instagram-clone/src/pages/Home.tsx
+++ artifacts/instagram-clone/src/pages/Home.tsx
@@ -251,7 +251,7 @@ export default memo(function Home() {
             >
               <div className="relative">
                 {isTangled && (
-                  <img src="/themes/camelon_cropped.png" alt="" className="absolute -top-3 -left-5 sm:-top-5 sm:-left-7 w-12 h-12 sm:w-16 sm:h-16 z-30 drop-shadow-md object-contain -rotate-12" />
+                  <img src="/themes/pascal_transparent.png" alt="" className="absolute -top-3 -left-5 sm:-top-5 sm:-left-7 w-12 h-12 sm:w-16 sm:h-16 z-30 drop-shadow-md object-contain -rotate-12" />
                 )}
                 {/* My avatar — tap to view/add story */}
                 <button
@@ -307,7 +307,7 @@ export default memo(function Home() {
 
               <div className="relative">
                 {isTangled && (
-                  <img src="/themes/camelon_cropped.png" alt="" className="absolute -top-3 -right-5 sm:-top-5 sm:-right-7 w-12 h-12 sm:w-16 sm:h-16 z-30 drop-shadow-md object-contain scale-x-[-1] rotate-12" />
+                  <img src="/themes/pascal_transparent.png" alt="" className="absolute -top-3 -right-5 sm:-top-5 sm:-right-7 w-12 h-12 sm:w-16 sm:h-16 z-30 drop-shadow-md object-contain scale-x-[-1] rotate-12" />
                 )}
                 {/* Partner avatar — tap to view story if they have one, else do nothing */}
                 <button
